22a47b9cf6f0817ae84a4c17f7342e6734ec4533
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-08-05 19:18:22 -0700

    my changes

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+url: string = 'http://andreia.com';
+cursoAngular: boolean = true;
+urlImagem = 'http://lorempixel.com/400/200/nature';
+
+valorAtual: string = ''; 
+valorSalvo = '';
+
+isMouseOver: boolean = false;
+
+nomeDoCurso: string = 'Angular';
+
+valorInicial = 15;
+
+nome: string = 'abc';
+
+pessoa: any ={
+  nome: 'def',
+  idade: 20
+}
+
+
+getValor(){
+  return 1;
+}
+
+getCurtirCurso(){
+  return true;
+}
+
+botaoClicado(){
+  alert('Botão Clicado!');
+}
+
+onKeyUp(evento: KeyboardEvent){
+  this.valorAtual = (<HTMLInputElement>evento.target).value;
+}
+
+salvarValor(valor){
+  this.valorSalvo = valor;
+}
+
+onMouseOverOut(){
+  this.isMouseOver = !this.isMouseOver;
+}
+
+onMudouValor(evento){
+  console.log(evento.novoValor);
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
